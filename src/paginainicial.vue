d817c983fbcfc9dafe02befb4273addf9c393fb7
santosjuliana23 <a.pkskj@gmail.com>
2024-04-08 15:24:09 -0300

    Init Project

---
+++ src/paginainicial.vue
@@ -0,0 +1,23 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+<h1>página inicial</h1>
+  <header>
+     <ul>
+        <li><a href="equipe.vue">Equipe</a></li>
+        <li><a href="projetos.vue">Projetos</a></li>
+        <li><a href="paginadecontato.vue">Contato</a></li>
+    </ul>
+  </header>
+
+  <main>
+   
+  </main>
+</template>
+
+<style scoped>
+
+</style>
